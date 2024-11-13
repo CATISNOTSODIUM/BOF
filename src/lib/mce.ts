8507c1e3f143b4d1ab770262ef0e83aae1124f0f
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2024-11-13 22:55:13 +0800

    create visual mce

--- src/lib/mce.ts
+++ src/lib/mce.ts
@@ -1,7 +1,8 @@
 import { bf_evaluate } from "./evaluator";
 import { lexing } from "./lexer";
 import { Result, unwrap, ok, is_error, error } from "./types";
-export function ParseAndEvaluate(source: string): Result<Number[], String> {
+
+export function ParseAndEvaluate(source: string, mem_return = false): Result<Number[] | [Number[], Number[]], String> {
     
     if (source.length == 0) {
         return error(`[PARSING ERROR] Expected at least one character.`);
@@ -10,7 +11,7 @@ export function ParseAndEvaluate(source: string): Result<Number[], String> {
     if (is_error(tokens)) {
         return error(`[PARSING ERROR] ${tokens.data}`);
     }
-    const evaluated = bf_evaluate(unwrap(tokens));
+    const evaluated = bf_evaluate(unwrap(tokens), mem_return);
     return ok(unwrap(evaluated));
 }
 
